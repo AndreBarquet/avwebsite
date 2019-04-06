095b33ac7a86835c54a901c8d9a66db88619bc77
AndreBarquet <andreluiz97.bm@hotmail.com>
2019-04-06 13:32:48 -0300

    Introducing Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CircularNavbar from '../src/Components/CircularNavbar/CircularNavbar';
+import { Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,21 +10,8 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
-=======
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >            
-          </a>
-          <CircularNavbar/>
->>>>>>> 924174817371fcc2501600beb673cc25c6311386
         </header>
+        <Link to="/curriculum"><button>Entrar</button></Link>
       </div>
     );
   }
