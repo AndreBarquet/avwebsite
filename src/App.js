079df9dccce39a3ffa12166e694ae9cf7f911a6e
AndreBarquet <andreluiz97.bm@hotmail.com>
2019-04-19 12:06:24 -0300

    almost complete circular navbar, just miss a few details

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React, { Component } from 'react';
-import CircularNavbar from './Components/CircularNavbar/CircularNavbar';
-import Card from './Components/Card/Card';
 import logo from './logo.svg';
+import CircularNavbar from './Components/CircularNavbar/CircularNavbar';
 import './App.css';
 
 class App extends Component {
   constructor(props){
-    super(props)
+    super(props);
+    
     
   }
   render() {
+    /*
     const dev = {   
       imglink: 'https://scontent.fuba1-1.fna.fbcdn.net/v/t1.0-9/36423294_1820525594729700_7667887280568139776_n.jpg?_nc_cat=105&_nc_ht=scontent.fuba1-1.fna&oh=4176a5ffcdb940ca48ee3e5af4d3b27d&oe=5D5045F8',
       name: 'Victor Hugo',
       city: 'Uberaba',
       areadev: 'Full-Stack Developer' ,
       skills: ['NodeJS', 'ExpressJS', 'ReactJS', 'Angular 2+', 'HTML/CSS3']     
-    }  
+    }  */
     //<Card dev={dev}/>
     return (
       <div className="App">
-      <CircularNavbar />
+        <CircularNavbar></CircularNavbar>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
-        <button>Entrar</button>
       </div>
     );
   }
--- src/Components/CircularNavbar/CircularNavbar.js
+++ src/Components/CircularNavbar/CircularNavbar.js
@@ -2,26 +2,41 @@ import React, { Component } from 'react';
 import '../CircularNavbar/CircularNavbar.css'
 
 class CircularNavbar extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = { 
+            isSwitchOn: false
+        }
+      }
+
+      // pode fazer o state dessa maneira, ou colocar no construtor, como definido acima
+      /*
+      state = {
+          isSwitchOn: false
+      }
+      */
+
+      handleClick() {
+        console.log('Clicouuu');
+        this.setState({
+          // funciona como um toggle, usando o "!" ele pega o oposto do que está no state definido
+          isSwitchOn: !this.state.isSwitchOn  
+        })
+        
+      }
     render() {
         return (
-            <div className="navbarBackground">
-                <label>
-                    <input type="checkbox"></input>
-                    <span class="menu">
-                        <span class="hamburger"></span>
-                    </span>
-                    <ul>
-                        
-                            <a href="#">Home</a>
-                        
-                        
-                            <a href="#">About</a>
-                        
-                        
-                            <a href="#">Work</a>
-                        
-                    </ul>
-                </label>
+            <div className={/* Verifica se isSwitchOn é igual a true*/ this.state.isSwitchOn ? "shazam" : "page"}>
+                <span className="menu_toggle" onClick={this.handleClick}>
+                    <i className="menu_open fa fa-bars fa-lg"></i>
+                    <i className="menu_close fa fa-times fa-lg"></i>
+                </span>
+                <ul className="menu_items">
+                    <li><a href="#"><i className="icon fa fa-home fa-2x"></i> Moar</a></li>
+                    <li><a href="#"><i className="icon fa fa-phone fa-2x"></i> Coffee</a></li>
+                    <li><a href="#"><i className="icon fa fa-heart fa-2x"></i> Please</a></li>
+                </ul>
             </div>
         );
     }
